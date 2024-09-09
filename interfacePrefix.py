ececbe94766da8140d3e76d4f7afbe007c4c47a7
zhouska <zbynek.houska@gmail.com>
2024-09-09 12:55:27 +0200

    Added sensor attributes for easier troubleshooting

--- interfacePrefix.py
+++ interfacePrefix.py
@@ -1,4 +1,4 @@
-import requests, ipaddress
+import requests, ipaddress, json
 from datetime import datetime
 
 # Define your credentials and endpoint URLs
@@ -13,6 +13,18 @@ edge_id = <your edge id>
 interface = "<your interface>"
 date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+def prepare_output(state, ipv6_address=None, message=None):
+    return json.dumps({
+        "state": state,
+        "attributes": {
+            "current_ipv6_address": ipv6_address,
+            "expected_prefix": prefix,
+            "interface": interface,
+            "last_check_time": date,
+            "message": message
+        }
+    })
+
 # Define the login payload
 login_payload = {
     "username": username,
@@ -26,9 +38,7 @@ session = requests.Session()
 login_response = session.post(login_url, json=login_payload)
 
 # Check if login was successful
-if login_response.status_code == 200:
-    print("Login successful")
-else:
+if not login_response.status_code == 200:
     print(f"Login failed: {login_response.status_code}")
     print(login_response.text)
     exit()
@@ -50,21 +60,19 @@ if edge_response.status_code == 200:
     link = next((iface for iface in edge_data.get("links", []) if iface.get("interface") == interface), None)
 
     if link:
-        ipv6_address = link.get("ipV6Address")
-        if ipv6_address:
-
-            # Compare the IPv6 address with your prefix
-            interface_ip = ipaddress.IPv6Address(ipv6_address)
-            prefix_network = ipaddress.IPv6Network(prefix)
-
-            if interface_ip in prefix_network:
-                print(f"{date} > Prefix matches")
-            else:
-                print(f"{date} > Prefix doesn't match")
-        else:
-            print(f"{date} > IPv6 address not found for {interface} interface.")
+           ipv6_address = link.get("ipV6Address")
+           if ipv6_address:
+               interface_ip = ipaddress.IPv6Address(ipv6_address)
+               prefix_network = ipaddress.IPv6Network(prefix)
+    
+               if interface_ip in prefix_network:
+                   print(prepare_output("Prefix matches", ipv6_address))
+               else:
+                   print(prepare_output("Prefix doesn't match", ipv6_address, 
+                                        f"IPv6 address found on the interface is {ipv6_address}, however, prefix should be {prefix}"))
+           else:
+               print(prepare_output("Error", message=f"IPv6 address not found for {interface} interface."))
     else:
-        print(f"{date} > {interface} interface not found.")
+        print(prepare_output("Error", message=f"{interface} interface not found."))
 else:
-    print(f"Failed to get edge details: {edge_response.status_code}")
-    print(edge_response.text)
+    print(prepare_output("Error", message=f"Failed to get edge details: {edge_response.status_code}"))
